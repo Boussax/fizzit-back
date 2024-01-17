e9f2d1104ba15e55ee4a114318ffdf0cfea7ef38
Boussax <adrien.boussaoud@gmail.com>
2024-01-17 11:19:56 +0100

    feat: get by id + delete + fix mock

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,20 @@ app.get('/', (req: Request, res: Response) => {
 app.get('/ferments', (req: Request, res: Response) => {
     res.json(ferments);
 });
+
+app.get('/ferment/:id', (req: Request, res: Response) => {
+    const id = Number(req.params.id)
+    const ferment = ferments.find(ferment => ferment.id === id)
+    res.json(ferment)
+});
+
+
+app.delete('/ferments/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const index = ferments.findIndex(ferment => ferment.id === id)
+        if (index === -1) {
+        return res.status(404).send('Ferment not found')
+    }
+    ferments.splice(index,1)
+    res.status(200).json('Ferment deleted')
+})
