fbdd86c45b43ae89fddfd405ae54ca0f00f72196
Boussax <adrien.boussaoud@gmail.com>
2024-01-17 20:29:28 +0100

    refacto: reorganized routes + implementation

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,14 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
-import { FERMENTS } from "../mock/mock-ferments";
-import { Ferment } from "../models/ferment";
-
+import { router as ferments_routes } from "./routes/ferments"
 dotenv.config();
-
-const ferments = FERMENTS;
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.json()) // parse json body content
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("Server running");
-});
-
-app.get('/ferments', (req: Request, res: Response) => {
-    res.json(ferments);
-});
-
-app.get('/ferment/:id', (req: Request, res: Response) => {
-    const id = Number(req.params.id)
-    const ferment = ferments.find(ferment => ferment.id === id)
-    res.json(ferment)
-});
-
-app.get('/ferments/query', (req: Request, res: Response) => {
-    const type = (req.query.type as string).toLowerCase()
-    const ferments_result = ferments.filter(ferment => ferment.type.toLowerCase().includes(type))
-
-    if (ferments_result.length < 1) {
-        return res.status(200).send('No ferments matched your search')
-    }
-    res.json(ferments_result)
-})
-
-app.post('/ferment', (req: Request, res: Response) => {
-    const newFerment : Ferment = {
-        id: ferments.length + 1,
-        name: req.body.name,
-        type : req.body.type ,
-        status: 'ongoing',
-        startDate : Date.now(),
-        fermentationDuration : 5,
-    };
-    ferments.push(newFerment);
-    res.status(201).json(newFerment);
-})
-
-app.put('/ferment/:id', (req: Request, res: Response) => {
-    const id = Number(req.params.id);
-    const index = ferments.findIndex(ferment => ferment.id === id);
-    if (index === -1) {
-        return res.status(404).send('Ferment not found')
-    }
-    const updatedFerment = {
-        id: ferments[index].id,
-        name: req.body.name,
-        type : req.body.type ,
-        status: req.body.status,
-        startDate : req.body.startDate,
-        fermentationDuration : req.body.fermentationDuration,
-    }
-    ferments[index] = updatedFerment
-    res.status(200).json('Ferment updated')
-})
-
-app.delete('/ferments/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const index = ferments.findIndex(ferment => ferment.id === id)
-        if (index === -1) {
-        return res.status(404).send('Ferment not found')
-    }
-    ferments.splice(index,1)
-    res.status(200).json('Ferment deleted')
-})
+app.use(express.json()) // parse json body content
+app.use('/ferments', ferments_routes)
