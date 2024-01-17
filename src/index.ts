620483b95c26aaaf6b7f9fa4d6085e1e3f134de2
Boussax <adrien.boussaoud@gmail.com>
2024-01-17 11:17:20 +0100

    feat: add model + mock data + get all

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { FERMENTS } from "../mock/mock-ferments";
+import { Ferment } from "../models/ferment";
 
 dotenv.config();
+
+const ferments = FERMENTS;
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Ou Server');
-});
+app.use(express.json()) // parse json body content
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Server running");
+});
+
+app.get('/ferments', (req: Request, res: Response) => {
+    res.json(ferments);
 });
