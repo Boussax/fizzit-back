afd751523a24f8390b979538e1c57b4e339f77e4
Boussax <88453290+Boussax@users.noreply.github.com>
2024-01-17 23:17:29 +0100

    Feat/add prettier (#3)
    * Feat/initiate crud app (#1)
    ** feat: add model + mock data + get all
    ** feat: get by id + delete + fix mock
    ** feat: create and update
    ** feat: add query by type
    ** refacto: reorganized routes + implementation
    
    * add prettier to dev dep

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
-
+import { router as ferments_routes } from "./routes/ferments";
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Ou Server');
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+app.use(express.json()); // parse json body content
+app.use("/ferments", ferments_routes);
---
+++ src/models/ferment.ts
@@ -0,0 +1,8 @@
+export interface Ferment {
+  id: number;
+  name: string;
+  type: string;
+  status: string;
+  startDate: number;
+  fermentationDuration: number;
+}
