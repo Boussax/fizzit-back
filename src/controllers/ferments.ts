060d493de5c6d10467135644110e9bc47f38e95c
Boussax <adrien.boussaoud@gmail.com>
2024-01-31 00:58:22 +0100

    connect create to db + clean update

--- src/controllers/ferments.ts
+++ src/controllers/ferments.ts
@@ -1,16 +1,14 @@
 import { Request, Response } from "express";
-import { FERMENTS } from "../mock/mock-ferments";
 import { Ferment } from "../models/ferment";
 import {
+  sql_create_new_ferment,
   sql_delete_ferment,
   sql_get_all,
   sql_get_one,
 } from "../database/queries";
 import { Pool } from "pg";
 
-
-const ferments = FERMENTS;
-
+// const ferments = FERMENTS;
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
@@ -18,6 +16,7 @@ const pool = new Pool({
   password: "3L15@L3mm0",
   port: 5432,
 });
+let fermentsCount: number = 0;
 export const getFerments = (req: Request, res: Response) => {
   pool
     .query(sql_get_all, [])
@@ -34,7 +33,7 @@ export const getFerments = (req: Request, res: Response) => {
         };
         ferments.push(ferment);
       });
-  res.json(ferments);
+      res.json(ferments);
     })
     .catch((error) => console.log(error));
 };
@@ -51,38 +50,51 @@ export const getFerment = (req: Request, res: Response) => {
         status: results.rows[0].status,
         fermentationDuration: results.rows[0].fermentationduration as number,
       };
-  res.json(ferment);
+      res.json(ferment);
     })
     .catch((error) => console.log(error));
 };
 
 export const createFerment = (req: Request, res: Response) => {
-  const newFerment: Ferment = {
-    id: ferments.length + 1,
-    name: req.body.name,
-    type: req.body.type,
-    status: "ongoing",
-    startDate: Date.now(),
-    fermentationDuration: 5,
-  };
-  ferments.push(newFerment);
-  res.status(201).json(newFerment);
+  console.log(fermentsCount);
+  pool
+    .query(sql_create_new_ferment, [
+      fermentsCount + 1,
+      req.body.name,
+      req.body.type,
+      "ongoing",
+      req.body.startDate,
+      req.body.fermentationDuration,
+    ])
+    .then((result) => {
+      console.log(result);
+      const createdFerment: Ferment = {
+        id: fermentsCount + 1,
+        name: req.body.name,
+        type: req.body.type,
+        status: "ongoing",
+        startDate: req.body.startDate,
+        fermentationDuration: req.body.fermentationDuration,
+      };
+      fermentsCount++;
+      res.status(201).json(createdFerment);
+    });
 };
 
-export const updateFerment = (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  const index = ferments.findIndex((ferment) => ferment.id === id);
-  const updatedFerment = {
-    id: ferments[index].id,
-    name: req.body.name,
-    type: req.body.type,
-    status: req.body.status,
-    startDate: req.body.startDate,
-    fermentationDuration: req.body.fermentationDuration,
-  };
-  ferments[index] = updatedFerment;
-  res.status(200).json("Ferment updated");
-};
+// export const updateFerment = (req: Request, res: Response) => {
+//   const id = Number(req.params.id);
+//   const index = ferments.findIndex((ferment) => ferment.id === id);
+//   const updatedFerment = {
+//     id: ferments[index].id,
+//     name: req.body.name,
+//     type: req.body.type,
+//     status: req.body.status,
+//     startDate: req.body.startDate,
+//     fermentationDuration: req.body.fermentationDuration,
+//   };
+//   ferments[index] = updatedFerment;
+//   res.status(200).json("Ferment updated");
+// };
 
 export const deleteFerment = (req: Request, res: Response) => {
   pool
--- src/database/queries.ts
+++ src/database/queries.ts
@@ -10,4 +10,6 @@ export const sql_create = `CREATE TABLE IF NOT EXISTS Ferments (
 export const sql_get_all = `SELECT * FROM Ferments`;
 export const sql_get_one = `SELECT * FROM Ferments WHERE ID=$1`;
 
+export const sql_create_new_ferment = `INSERT INTO Ferments VALUES ($1, $2, $3, $4, $5, $6);`;
+
 export const sql_delete_ferment = `DELETE from Ferments WHERE ID=$1`;
