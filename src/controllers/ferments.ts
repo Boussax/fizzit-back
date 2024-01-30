fdea0ef860400e9f861fb9f0402e735b97ffe2ce
Boussax <adrien.boussaoud@gmail.com>
2024-01-31 00:54:24 +0100

    get from db

--- src/controllers/ferments.ts
+++ src/controllers/ferments.ts
@@ -1,21 +1,58 @@
 import { Request, Response } from "express";
 import { FERMENTS } from "../mock/mock-ferments";
 import { Ferment } from "../models/ferment";
+import {
+  sql_get_all,
+  sql_get_one,
+} from "../database/queries";
+import { Pool } from "pg";
+
 
 const ferments = FERMENTS;
 
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "fizzit_data",
+  password: "3L15@L3mm0",
+  port: 5432,
+});
 export const getFerments = (req: Request, res: Response) => {
+  pool
+    .query(sql_get_all, [])
+    .then((results) => {
+      const ferments: Ferment[] = [];
+      results.rows.forEach((row) => {
+        const ferment: Ferment = {
+          id: row.id as number,
+          name: row.name,
+          type: row.type,
+          startDate: row.startdate,
+          status: row.status,
+          fermentationDuration: row.fermentationduration as number,
+        };
+        ferments.push(ferment);
+      });
   res.json(ferments);
+    })
+    .catch((error) => console.log(error));
 };
 
 export const getFerment = (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  const ferment = ferments.find((ferment) => ferment.id === id);
-
-  if (!ferment) {
-    return res.status(404).send("Ferment not found");
-  }
+  pool
+    .query(sql_get_one, [req.params.id])
+    .then((results) => {
+      const ferment: Ferment = {
+        id: results.rows[0].id as number,
+        name: results.rows[0].name,
+        type: results.rows[0].type,
+        startDate: results.rows[0].startdate,
+        status: results.rows[0].status,
+        fermentationDuration: results.rows[0].fermentationduration as number,
+      };
   res.json(ferment);
+    })
+    .catch((error) => console.log(error));
 };
 
 export const createFerment = (req: Request, res: Response) => {
--- src/database/queries.ts
+++ src/database/queries.ts
@@ -7,3 +7,6 @@ export const sql_create = `CREATE TABLE IF NOT EXISTS Ferments (
     FermentationDuration INTEGER NOT NULL
   );`;
 
+export const sql_get_all = `SELECT * FROM Ferments`;
+export const sql_get_one = `SELECT * FROM Ferments WHERE ID=$1`;
+
